b8933cc7388aa52c8b6329ed7a3ca431c00890ef
HP ELITEBOOK <HP ELITEBOOK@DESKTOP-30TR2FF>
2023-09-01 13:02:21 +0200

    tallyta

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -5,14 +5,20 @@ namespace Tachyon.Models
 {
     public class Booking
     {
-        
-           [Key]
-            public int id { get; set; }
-            public string? name { get; set; }
-            public string? lastname { get; set; }
-            public string? email { get; set; }
-            public DateTime? datetimevalue { get; set; }
-            public string? subsystem { get; set; }
 
-        }
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string lastname { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public DateTime datetimevalue { get; set; }
+        [Required]
+        public string Department { get; set; }
+
+
     }
+}
